82a3feb599cb47838b0fe0bfd016f46cd1446831
lhmet <lhmet@ufsm.br>
2018-02-17 22:45:59 -0200

    rev text

---
+++ R/hidro_metadata.R
@@ -0,0 +1,17 @@
+#' Metadata of hydrological stations on Hidroweb database
+#'
+#' A dataset containing coordinates and additional attributes of 46036
+#'  hydrological stations
+#'
+#' @format A data frame with 46036 rows and 13 variables:
+#' \describe{
+#' \item{Codigo}{station code ID}
+#' \item{Longitude}{degrees}
+#' \item{Latitude}{degrees}
+#' \item{Altitude}{masl}
+#' \item{AreaDrenagem}{km^-2^}
+#' \item{Nome}{station name}
+#' ...
+#' }
+#' @source \url{http://www.diamondse.info/}
+"hidro_metadata"
