8c9daab2dfdb90a93e5a50c61e9ec08783abf738
lhmet <lhmet@ufsm.br>
2018-02-18 00:38:14 -0300

    update doc

--- R/hidro_metadata.R
+++ R/hidro_metadata.R
@@ -10,12 +10,17 @@
 #' @format A data frame with 46036 rows and 13 variables:
 #' \describe{
 #' \item{Codigo}{station code ID}
-#' \item{Longitude}{degrees}
-#' \item{Latitude}{degrees}
+#' \item{Longitude}{ in decimal degrees}
+#' \item{Latitude}{in decimal degrees}
 #' \item{Altitude}{masl}
-#' \item{AreaDrenagem}{km^-2^}
-#' \item{Nome}{station name}
-#' ...
+#' \item{AreaDrenagem}{Drainage area, Km2}
+#' \item{TipoEstacao}{station type}
+#' \item{Municipio}{city}
+#' \item{Estado}{state name}
+#' \item{UF}{Federal Unit}
+#' \item{Bacia}{river basin}
+#' \item{SubBacia}{sub basin}
+#' \item{Rio}{river name}
 #' }
 #' @source \url{http://hidroweb.ana.gov.br/HidroWeb.asp?TocItem=6010#inventario}
 "hidro_metadata"
