9eb6e30acc6049939c56a0a69bbb510d6cc49f23
lhmet <lhmet@ufsm.br>
2018-02-18 03:31:16 -0300

    under construction

---
+++ R/hidroweb.R
@@ -0,0 +1,17 @@
+# hidroweb_down <- function(code,
+#                           data.type = c(
+#                             "Cota", "Vazao", "Chuva",
+#                             "Qualidade", "Resumo", "Sedimento",
+#                             "Perfil"
+#                           ),
+#                           verbose,
+#                           dest.dir = "../",
+#                           ){
+#   # change args names
+#   purrr::map_df(id, ~.get_hidroweb(.x, 
+#                                    option = data.type,
+#                                    .verbose = verbose,
+#                                    .dest.dir = dest.dir
+#                                    )
+#                 )
+# }
