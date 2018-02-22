0e25bf8f599c3b76976b714f59316c65abf3e4da
lhmet <lhmet@ufsm.br>
2018-02-22 02:09:16 -0300

    review spell

--- R/hidroweb.R
+++ R/hidroweb.R
@@ -4,27 +4,37 @@
 #' @param stations character vector with code of stations
 #' @param options character vector with one or more options: "Cota", "Vazao",
 #'  "Chuva", "Qualidade", "Resumo", "Sedimento", "Perfil".
+#' @param combo Logical, if TRUE a unique combination of all elements of 
+#' stations and options is used. Otherwise, donwload is executed to each
+#' station-option pair. 
 #' @param verbose logical. if TRUE R report extra information on progress.
 #' @param dest.dir A character string with the path where the downloaded file 
 #' is saved.
 #' @param meta logical, if TRUE return information including station metadata. 
 #'
-#' @return tibble with information of dowloaded stations files.
+#' @return a \code{tibble} with information of downloaded stations files.
 #' @export
 #'
 hidroweb_down <- function(stations = "42450750",
                           options = "Chuva",
+                          combo = TRUE,
                           verbose = TRUE,
                           dest.dir = "./",
                           meta = TRUE) {
 
   # check inputs
 
-  # options and stn combinations
-  arg_combs <- expand.grid(options, stations, stringsAsFactors = FALSE)
-  arg_combs <- setNames(arg_combs, nm = c("options", "stations"))
-  stns_l <- arg_combs$stations
-  opts_l <- arg_combs$options
+  if(combo){
+    # options and stn combinations
+    arg_combs <- expand.grid(options, stations, stringsAsFactors = FALSE)
+    arg_combs <- setNames(arg_combs, nm = c("options", "stations"))
+    stns_l <- arg_combs$stations
+    opts_l <- arg_combs$options  
+  } else {
+    stopifnot(length(stations) == length(options))
+    stns_l <- stations
+    opts_l <- options  
+  }
   
   purrr::map2_df(
     stns_l, opts_l,
